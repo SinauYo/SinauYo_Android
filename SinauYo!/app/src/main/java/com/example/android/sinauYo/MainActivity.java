ce09695c14e8545601026f78995b0c708da84f6d
ranggaayesha <36043909+ranggaayesha@users.noreply.github.com>
2018-05-03 17:53:11 +0700

    A2 - Navigation bar

--- SinauYo!/app/src/main/java/com/example/android/sinauYo/MainActivity.java
+++ SinauYo!/app/src/main/java/com/example/android/sinauYo/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.android.sinauYo;
 
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.ClipData;
 import android.content.Intent;
 import android.graphics.Color;
@@ -20,7 +18,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {    //
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
@@ -37,41 +35,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
 
-        NavigationView nav=(NavigationView)findViewById(R.id.navigation_view);
-        nav.setNavigationItemSelectedListener(this);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView nav=(NavigationView)findViewById(R.id.navigation_view);  //
+        nav.setNavigationItemSelectedListener(this);    //
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);  //
 
 
         //actionBar = getSupportActionBar();
         //actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#32be98")));
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        MenuInflater inflater=getMenuInflater();
-//        inflater.inflate(R.menu.navigation,menu);
-//        return super.onCreateOptionsMenu(menu);
-//    }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.navigation, menu);
         return true;
     }
 
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        //Mendapatkan id dari item yang
-//        int id = item.getItemId();
-//        //Apabila item yang dipilih adalah setting
-//        if (id==R.id.navigation_logout){
-//            //Membuat intent baru dari list to do ke pengaturan
-//            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-//            //Memulai intent
-//            startActivity(intent);
-//            //Menutup activity setelah berhasil dilakukan
-//            finish();
-//        }
-//        return true;
-//    }
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
@@ -89,48 +67,6 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     }
 
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        int id =item.getItemId();
-//        if (id==R.id.navigation_logout){
-//            onLogout();
-//        }
-////        if(mToggle.onOptionsItemSelected(item)){
-////            return true;
-////        }
-//        return super.onOptionsItemSelected(item);
-//    }
-
-//    public void onLogout() {
-//            Toast.makeText(this, "See you later!", Toast.LENGTH_SHORT).show();
-//            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-//            startActivity(intent);
-//    }
-
-//    public void selectItem(MenuItem item){
-//        Fragment myfragment =null;
-//        Class fragmentClass;
-//        switch (item.getItemId()){
-//            case R.id.navigation_logout:
-//            fragmentClass=LoginActivity.class;
-//            break;
-//
-//            default:
-//                fragmentClass=MainActivity.class;
-//        }
-//        try {
-//            myfragment=(Fragment)fragmentClass.newInstance();
-//
-//        }
-//        catch (Exception e){
-//            e.printStackTrace();
-//        }
-//        FragmentManager manager= getSupportFragmentManager();
-//        manager.beginTransaction().replace(R.id.item_view,myfragment).commit();
-//        item.setChecked(true);
-//        setTitle(item.getTitle());
-//        mDrawerLayout.closeDrawers();
-//    }
 
 
 
